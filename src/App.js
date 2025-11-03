37afd5616879487a014564cd54781096f948ba22
Lim Yeong-Bin <dudqls327@naver.com>
2025-11-03 16:18:08 +0900

    test

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import "../src/index.css"
 import { useState, useEffect, useRef } from 'react';
 
 function App() {
-  const [PuzzleStatus, setPuzzleStatus] = useState(3);
+  const [PuzzleStatus, setPuzzleStatus] = useState(1);
 
   const [total, settotal] = useState({
     total_users: null,
